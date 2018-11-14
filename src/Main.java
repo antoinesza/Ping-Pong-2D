164c0aa324cfe6a8329c85027d677038d75b5e24
antoine szajryck <antoine.szajryck@gmail.com>
2018-11-14 14:23:57 +0100

    first

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        JFrame fenetre = new JFrame();
+
+        fenetre.setTitle("Ping Pong 2D AntoineSza");
+        fenetre.setSize(800, 600);
+        fenetre.setResizable(false);
+        fenetre.setLocationRelativeTo(null);
+
+
+        fenetre.setVisible(true);
+    }
+}
