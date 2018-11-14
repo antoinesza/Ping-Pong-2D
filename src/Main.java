1ff3c01b4004b34159a0cc3b971f832676cb2036
antoine szajryck <antoine.szajryck@gmail.com>
2018-11-14 14:33:56 +0100

    Objets jeu

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import javax.swing.JFrame;
 
 public class Main
 {
+    public static ObjetsJeu panel = new ObjetsJeu();
+
     public static void main(String[] args)
     {
         JFrame fenetre = new JFrame();
--- src/ObjetsJeu.java
+++ src/ObjetsJeu.java
@@ -1,2 +1,25 @@
-public class ObjetsJeu {
+import java.awt.color;
+import java.awt.Graphics;
+import java.awt.JPanel;
+
+
+
+public class ObjetsJeu extends JPanel
+{
+    private int joueurX;
+    private int joueury;
+
+
+    private int ennemiX;
+    private int ennemiY;
+
+    public ObjetsJeu()
+    {
+        joueurX = 10;
+        joueurX = 10;
+
+    }
+
+
+
 }
