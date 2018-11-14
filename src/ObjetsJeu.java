4f0f8db00456f7d48faf6bc6fc18ff5ed889f636
antoine szajryck <antoine.szajryck@gmail.com>
2018-11-14 15:06:17 +0100

    debug

--- src/ObjetsJeu.java
+++ src/ObjetsJeu.java
@@ -1,14 +1,11 @@
-import java.awt.color;
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.JPanel;
-
-
+import javax.swing.JPanel;
 
 public class ObjetsJeu extends JPanel
 {
     private int joueurX;
-    private int joueury;
-
+    private int joueurY;
 
     private int ennemiX;
     private int ennemiY;
@@ -16,10 +13,20 @@ public class ObjetsJeu extends JPanel
     public ObjetsJeu()
     {
         joueurX = 10;
-        joueurX = 10;
+        joueurY = 235;
 
+        ennemiX=  744;
     }
 
+    public void paintComponent(Graphics g)
+    {
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
 
+        g.setColor(Color.RED);
+        g.fillRect(joueurX, joueurY, 40, 100);
 
+        g.setColor(Color.GREEN);
+        g.fillRect(ennemiX, ennemiY, 40, 100);
+    }
 }
